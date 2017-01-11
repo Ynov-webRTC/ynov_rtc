ca6948c240c6100a136012301fcb6ccbc92c9288
Florimond JOUFFROY <florimond.jouffroy@gmail.com>
2017-01-11 11:49:55 +0100

    add dependeces mongoose and connexion

--- server.js
+++ server.js
@@ -357,6 +357,25 @@ function onIceCandidate(sessionId, _candidate) {
     }
 }
 
+function connexionUser() {
+	var passport = require('passport')
+		, LocalStrategy = require('passport-local').Strategy;
+
+	passport.use(new LocalStrategy(
+		function(username, password, done) {
+			User.findOne({ username: username }, function(err, user) {
+				if (err) { return done(err); }
+				if (!user) {
+					return done(null, false, { message: 'Incorrect username.' });
+				}
+				if (!user.validPassword(password)) {
+					return done(null, false, { message: 'Incorrect password.' });
+				}
+				return done(null, user);
+			});
+		}
+	));
+}
 app.use(express.static(path.join(__dirname, 'static')));
 
 app.set("views", './views');
