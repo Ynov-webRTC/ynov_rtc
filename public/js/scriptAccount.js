7133172dd3d49c892aba249c50f42fc441ac3036
Florimond JOUFFROY <florimond.jouffroy@gmail.com>
2017-02-23 10:44:41 +0100

    Update user

--- public/js/scriptAccount.js
+++ public/js/scriptAccount.js
@@ -1,7 +1,8 @@
 'use strict';
 
 $(document).ready(function() {
-
+    $('.alert').fadeTo(2000, 500).slideUp(500);
+    
     $('#edit').on('click', function () {
         $('.inputUpdate').each(function () {
             $(this).attr('disabled', false);
